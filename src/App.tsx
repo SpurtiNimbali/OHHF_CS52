36fb285439f3f9ea273072c1ba669e7767502e63
amritamalhotra <amrita.malhotra04@gmail.com>
2026-04-20 23:20:51 -0700

    welcomepage1
    added onboarding dots, added text and continue button to original page, added text and age options for second page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { WelcomeScreen } from './WelcomeScreen'
+import { WelcomeScreen } from './welcomeScreen'
 
 function App() {
   return <WelcomeScreen />
