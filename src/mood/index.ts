eb6b56c08b68875c5480887c5b9b059be9810bfb
Edwin Pena Soriano <pena04@stanford.edu>
2026-05-26 15:22:27 -0700

    Move latest-mood wellness recommendations to wellness page

--- src/mood/index.ts
+++ src/mood/index.ts
@@ -12,8 +12,11 @@ export {
 export { getMoodMessage, getChatPromptHint, getMoodChatPrefill } from './moodCopy'
 export {
   MOOD_PRIMARY_WELLNESS_TOOL,
+  MOOD_WELLNESS_HOME_DESCRIPTION,
   MOOD_SUGGESTED_EXERCISES,
   MOOD_WELLNESS_PRIMARY_SECONDARY,
+  MOOD_HOME_CARD_ORDER,
+  DEFAULT_HOME_CARD_ORDER,
   resolveSuggestedExercisesForMood,
   wellnessToolPath,
   isWellnessToolId,
@@ -23,14 +26,3 @@ export type { MoodCheckInChatState } from './moodCheckInNav'
 export { isMoodCheckInChatState } from './moodCheckInNav'
 export { MoodProvider, useMood, moodLocalDateKey, type MoodContextValue } from './MoodContext'
 export { MoodHeartFill } from './MoodHeartFill'
-export {
-  MOOD_PRIMARY_WELLNESS_TOOL,
-  MOOD_WELLNESS_HOME_DESCRIPTION,
-  MOOD_SUGGESTED_EXERCISES,
-  MOOD_WELLNESS_PRIMARY_SECONDARY,
-  MOOD_HOME_CARD_ORDER,
-  DEFAULT_HOME_CARD_ORDER,
-  wellnessToolPath,
-  isWellnessToolId,
-  resolveSuggestedExercisesForMood,
-} from './moodRecommendations'
